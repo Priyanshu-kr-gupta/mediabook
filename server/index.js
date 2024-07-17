f0fb09f33bdbe53f63ade96246cbf6f7d8f52dd2
priyanshu kumar <dlnpriyanshu@gmail.com>
2024-07-17 07:44:38 +0530

    server testing 3

--- server/index.js
+++ server/index.js
@@ -1,28 +1,33 @@
-const connectDb=require('./db/Connect')
 const express=require('express')
-var cors = require('cors')
 require('dotenv').config();
+var cors = require('cors')
+const app=express();
+const port= process.env.PORT || 5000;
+const connectDb=require('./db/Connect')
 const bodyParser = require('body-parser');
 const dblink=process.env.DBLINK || "mongodb://localhost:27017/mediabook"
 // const dblink="mongodb://localhost:27017/mediabook"
-connectDb(dblink)
-const app=express();
+app.use(
+  cors()
+);
 
-const port= process.env.PORT || 5000;
 
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 app.use(express.json({ limit: '50mb', extended: true }))
 app.use(express.json())
-app.use(
-  cors()
-);
+
+app.use((req, res, next) => {
+  console.log(`Request received: ${req.method} ${req.url}`);
+  next();
+});
+
 
 app.use('/api/auth',require("./routes/Auth"))
 app.use('/api/notes',require("./routes/Notes"))
 app.use('/api/user',require('./routes/Search'))
 
 app.get('/',(req,res)=>{
-    res.send("<h1>Hello World</h1>")
+  res.send("<h1>Hello World</h1>")
 })
 app.get('/api',(req,res)=>{
   res.send("<h1>Hello API</h1>")
@@ -30,6 +35,7 @@ app.get('/api',(req,res)=>{
 
 // running port
 app.listen(port,()=>{
-    console.log(" App is succesfully running on port " + port)
+  console.log(" App is succesfully running on port " + port)
 })
+connectDb(dblink)
 
