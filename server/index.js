cc405be2e52e1be2ae203c3eb86dcb788acda54d
Priyanshu Kumar <114975117+Priyanshu-kr-gupta@users.noreply.github.com>
2024-07-17 14:50:42 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -16,22 +16,13 @@ app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 app.use(express.json({ limit: '50mb', extended: true }))
 app.use(express.json())
 
-app.use((req, res, next) => {
-  console.log(`Request received: ${req.method} ${req.url}`);
-  next();
-});
 
 
 app.use('/api/auth',require("./routes/Auth"))
 app.use('/api/notes',require("./routes/Notes"))
 app.use('/api/user',require('./routes/Search'))
 
-app.get('/',(req,res)=>{
-  res.send("<h1>Hello World</h1>")
-})
-app.get('/api',(req,res)=>{
-  res.send("<h1>Hello API</h1>")
-})
+
 
 // running port
 app.listen(port,()=>{
